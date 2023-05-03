6eb80ac38f2f1818f05458bf5dde8b9762aaaddc
rater7tara <nowshinakteremu005@gmail.com>
2023-05-03 11:16:30 +0600

    app user cors added

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const port = process.env.PORT || 5000;
 const chefs = require('./data/chefs.json');
 
 app.use(cors());
+
+app.get('/', (req, res) =>{
+    res.send('chef recipes')
+});
+
+app.get('/chefs', (req, res) =>{
+    res.send(chefs);
+})
+
+app.listen(port, () =>{
+    console.log(`chef API is running on port: ${port}`)
+})
